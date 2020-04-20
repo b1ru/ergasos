f7ace3f5e56a1fe1907b5ecf8838e2cd99292070
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-04-20 19:59:13 +0300

    My_Applications and My_Listings V2 (Delete button works)

--- js/my_applications.js
+++ js/my_applications.js
@@ -1,3 +1,33 @@
-function delete_listing(event){
-  console.log(event);
-}
+$('#myModal').on('shown.bs.modal', function (e) {
+  var button = e.relatedTarget
+  $('#confirmbtn').on('click', function () {
+    var card = button.parentNode.parentNode.parentNode
+    if (card == null) {
+      return
+    }
+
+    var number = card.getElementsByClassName('card-header')[0].id.slice(-1)
+    while (card.firstChild) {
+      card.removeChild(card.firstChild)
+    }
+
+    var orgcard = card
+
+    if (card.nextElementSibling != null) {
+      while (card.nextElementSibling) {
+        card = card.nextElementSibling
+        var header = card.getElementsByClassName('card-header')[0]
+        var string = 'app' + number
+        header.id = string
+        string = 'Application #' + number
+        header.innerHTML = string
+        number++
+      }
+    }
+    orgcard.parentNode.removeChild(orgcard)
+  })
+})
+
+$('#myModal').on('hidden.bs.modal', function () {
+  $('#confirmbtn').off('click')
+})
