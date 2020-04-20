9a3e54d3bf25b59128ec97ddb061639cb774bcdc
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-04-20 20:27:47 +0300

    Small refactoring in my_profile

--- js/my_profile.js
+++ js/my_profile.js
@@ -1,51 +1,47 @@
-function upload_pressed(){
-  document.getElementById("uploadfile").click();
-  return false;
-}
+$('#uploadbtn').on('click', function () {
+  document.getElementById('uploadfile').click()
+})
 
-function edit_pressed(){
-  //Replace text with input boxes
-  var elements = [].slice.call(document.getElementsByClassName("desc"));
-  var i,newtag;
-  for(i = 0; i < 4; i++){
-    newtag = document.createElement("input");
-    newtag.setAttribute("type","text");
-    newtag.setAttribute("class","input_text");
-    newtag.setAttribute("placeholder",elements[i].innerHTML);
-    elements[i].innerHTML = "";
-    elements[i].appendChild(newtag);
+$('#editbtn').on('click', function () {
+  // Replace text with input boxes
+  var elements = [].slice.call(document.getElementsByClassName('desc'))
+  var i, newtag
+  for (i = 0; i < 4; i++) {
+    newtag = document.createElement('input')
+    newtag.setAttribute('type', 'text')
+    newtag.setAttribute('class', 'input_text')
+    newtag.setAttribute('placeholder', elements[i].innerHTML)
+    elements[i].innerHTML = ''
+    elements[i].appendChild(newtag)
   }
 
   // hide edit link and icon
-  document.getElementById("edit_link").style.display="none";
-  document.getElementById("pen_icon").style.display="none";
+  document.getElementById('editbtn').style.display = 'none'
+  document.getElementById('pen_icon').style.display = 'none'
 
-  //show save changes button
-  document.getElementById("save_link").style.display="inline";
-}
+  // show save changes button
+  document.getElementById('savebtn').style.display = 'inline'
+})
 
-function save_pressed(){
-  //hide save changes button
-  document.getElementById("save_link").style.display="none";
+$('#savebtn').on('click', function () {
+  // hide save changes button
+  document.getElementById('savebtn').style.display = 'none'
 
   // hide edit link and icon
-  document.getElementById("edit_link").style.display="inline";
-  document.getElementById("pen_icon").style.display="inline";
+  document.getElementById('editbtn').style.display = 'inline'
+  document.getElementById('pen_icon').style.display = 'inline'
 
-  //remove input boxes
-  var elements = [].slice.call(document.getElementsByClassName("desc"));
-  var changes = [].slice.call(document.getElementsByClassName("input_text"));
-  var text;
-  for(var i = 0; i < 4; i++){
-    if(changes[i].value){
-      text = changes[i].value;
+  // remove input boxes
+  var elements = [].slice.call(document.getElementsByClassName('desc'))
+  var changes = [].slice.call(document.getElementsByClassName('input_text'))
+  var text
+  for (var i = 0; i < 4; i++) {
+    if (changes[i].value) {
+      text = changes[i].value
+    } else {
+      text = changes[i].placeholder
     }
-    else{
-      text = changes[i].placeholder;
-    }
-    elements[i].appendChild(document.createTextNode(text));
-    changes[i].parentNode.removeChild(changes[i]);
+    elements[i].appendChild(document.createTextNode(text))
+    changes[i].parentNode.removeChild(changes[i])
   }
-
-
-}
+})
