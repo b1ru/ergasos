5a120304b99ed104e6af68be8dc1b10c3e3f1a61
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-04-22 00:32:25 +0300

    Added remove tag capability in create_listing

--- js/create_listing.js
+++ js/create_listing.js
@@ -6,8 +6,21 @@ document.getElementById('addtagbtn').addEventListener('click', function () {
     var newbtn = document.createElement('button')
     newbtn.setAttribute('type', 'button')
     newbtn.setAttribute('class', 'btn btn-info btn-sm tag')
+    newbtn.setAttribute('id', tag)
     newbtn.innerHTML = tag
     addbtn.nextElementSibling.appendChild(newbtn)
     addbtn.previousElementSibling.value = null
   }
+})
+
+$('#tagspan').on('mouseover', '.tag', function () {
+  $(this).css('text-decoration', 'line-through')
+})
+
+$('#tagspan').on('mouseout', '.tag', function () {
+  $(this).css('text-decoration', 'none')
+})
+
+$('#tagspan').on('click', '.tag', function () {
+  this.parentNode.removeChild(this)
 })
