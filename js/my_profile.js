0c4035c02b93c8e46a720c8719bf103c8f65302a
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-04-22 20:39:34 +0300

    Refactored my_profile.js

--- js/my_profile.js
+++ js/my_profile.js
@@ -1,38 +1,39 @@
-$('#uploadbtn').on('click', function () {
-  document.getElementById('uploadfile').click()
-})
+function saveButtonIsShown () {
+  return document.getElementById('savebtn').style.display == 'inline'
+}
 
-$('#editbtn').on('click', function () {
-  // Replace text with input boxes
+
+function toggleButtons () {
+  if(saveButtonIsShown()) {
+    document.getElementById('savebtn').style.display = 'none'
+    document.getElementById('editbtn').style.display = 'inline'
+    document.getElementById('pen_icon').style.display = 'inline'
+  } else {
+    document.getElementById('savebtn').style.display = 'inline'
+    document.getElementById('editbtn').style.display = 'none'
+    document.getElementById('pen_icon').style.display = 'none'
+  }
+}
+
+function createInput (value) {
+  var newtag = document.createElement('input')
+  newtag.setAttribute('type', 'text')
+  newtag.setAttribute('class', 'input_text')
+  newtag.setAttribute('placeholder', value)
+  newtag.setAttribute('value', value)
+  return newtag
+}
+
+function createInputBoxes () {
   var elements = [].slice.call(document.getElementsByClassName('desc'))
-  var i, newtag
-  for (i = 0; i < 4; i++) {
-    newtag = document.createElement('input')
-    newtag.setAttribute('type', 'text')
-    newtag.setAttribute('class', 'input_text')
-    newtag.setAttribute('placeholder', elements[i].innerHTML)
-    newtag.setAttribute('value', elements[i].innerHTML)
+  for (var i = 0; i < 4; i++) {
+    var newtag = createInput(elements[i].innerHTML)
     elements[i].innerHTML = ''
     elements[i].appendChild(newtag)
   }
+}
 
-  // hide edit link and icon
-  document.getElementById('editbtn').style.display = 'none'
-  document.getElementById('pen_icon').style.display = 'none'
-
-  // show save changes button
-  document.getElementById('savebtn').style.display = 'inline'
-})
-
-$('#savebtn').on('click', function () {
-  // hide save changes button
-  document.getElementById('savebtn').style.display = 'none'
-
-  // hide edit link and icon
-  document.getElementById('editbtn').style.display = 'inline'
-  document.getElementById('pen_icon').style.display = 'inline'
-
-  // remove input boxes
+function removeInputBoxes () {
   var elements = [].slice.call(document.getElementsByClassName('desc'))
   var changes = [].slice.call(document.getElementsByClassName('input_text'))
   var text
@@ -45,6 +46,22 @@ $('#savebtn').on('click', function () {
     elements[i].appendChild(document.createTextNode(text))
     changes[i].parentNode.removeChild(changes[i])
   }
+}
 
+/*
+  Button events
+*/
+$('#editbtn').on('click', function () {
+  createInputBoxes()
+  toggleButtons()
+})
+
+$('#savebtn').on('click', function () {
+  toggleButtons()
+  removeInputBoxes()
   toggleAlert()
+})
+
+$('#uploadbtn').on('click', function () {
+  document.getElementById('uploadfile').click()
 })
