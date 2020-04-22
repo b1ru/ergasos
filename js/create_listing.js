0eff5ada6524ba14e8ca6bfd53e4d4f23c13f4c5
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-04-22 20:21:15 +0300

    Refactored create_listing.js

--- js/create_listing.js
+++ js/create_listing.js
@@ -1,18 +1,28 @@
+function createTagButton (tag) {
+  var newbtn = document.createElement('button')
+  newbtn.setAttribute('type', 'button')
+  newbtn.setAttribute('class', 'btn btn-info btn-sm tag')
+  newbtn.setAttribute('id', tag)
+  newbtn.innerHTML = tag
+  return newbtn
+}
+
+function tagInputHasText () {
+  return document.getElementById('taginput').value
+}
+
+function eraseTagInput () {
+  document.getElementById('taginput').value = null
+}
+
 $('#addtagbtn').on('click', function () {
   var addbtn = document.getElementById('addtagbtn');
-  if (addbtn.previousElementSibling.value) {
-    var tag = addbtn.previousElementSibling.value
-
-    var newbtn = document.createElement('button')
-    newbtn.setAttribute('type', 'button')
-    newbtn.setAttribute('class', 'btn btn-info btn-sm tag')
-    newbtn.setAttribute('id', tag)
-    newbtn.innerHTML = tag
-    addbtn.nextElementSibling.appendChild(newbtn)
-    addbtn.previousElementSibling.value = null
+  if (tagInputHasText()) {
+    var tag = document.getElementById('taginput').value
+    document.getElementById('tagspan').appendChild(createTagButton(tag))
+    eraseTagInput()
   }
 })
-
 
 /*
   Tag button events
