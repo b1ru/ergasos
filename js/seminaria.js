e1b1599e03879c6ae2988a6fbc6a2a1c58f261e8
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-04-24 22:42:18 +0300

    seminaria test 2

--- js/seminaria.js
+++ js/seminaria.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
-  var w = Math.round($('.col-3').width());
+  var w = Math.round($('.col-3').width() - 20);
   $('.fb-post').attr('data-width', w);
+  $('.rightside').css('overflow-x', 'scroll');
 });
