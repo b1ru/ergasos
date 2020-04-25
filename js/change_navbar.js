6bda3ee944fd1e247240041fd6d01b425fb1c5c2
Bill Tsek <billtsekev@gmail.com>
2020-04-25 18:49:59 +0300

    Add functionality to dropdown's links

--- js/change_navbar.js
+++ js/change_navbar.js
@@ -9,10 +9,10 @@ function change_nav() {
 	new_div.innerHTML = `
 		<button class="btn btn-secondary  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
   <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">My Applications</a>
-    <a class="dropdown-item" href="#">My Listings</a>
-    <a class="dropdown-item" href="#">My Profile</a>
-    <a class="dropdown-item" href="#">Settings</a>
+    <a class="dropdown-item" href="./my_applications.html">My Applications</a>
+    <a class="dropdown-item" href="./my_listings.html">My Listings</a>
+    <a class="dropdown-item" href="./my_profile.html">My Profile</a>
+    <a class="dropdown-item" href="./settings.html">Settings</a>
 		<div class="dropdown-divider"></div>
     <a class="dropdown-item" href="#">Logout</a>
   </div>
