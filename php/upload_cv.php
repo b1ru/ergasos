db79ed961c60fe8798a5e0081c83c4d3add5bf31
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-06-08 20:18:35 +0300

    Upload + Download functional

--- php/upload_cv.php
+++ php/upload_cv.php
@@ -1,7 +1,10 @@
 <?php
-  $target_dir = "C:/xampp/uploads/";
+  $target_dir = "C:/xampp/htdocs/ergasos/uploads/";
   $newfilename = "cv_1.pdf";
   $target_file = $target_dir . $newfilename;
   move_uploaded_file($_FILES["cvfile"]["tmp_name"],$target_file);
+  $con = mysqli_connect("localhost","root","","ergasos");
+  $query = "UPDATE account_details SET cv='uploads/cv_1.pdf' WHERE userID = 1";
+  mysqli_query($con,$query);
   header("Location: {$_SERVER["HTTP_REFERER"]}");
 ?>
