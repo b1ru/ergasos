7cdf0bbe494c842eccf414221ea2d7ed3603590d
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-06-14 18:40:20 +0300

    Settings checks improved

--- js/main_scripts.js
+++ js/main_scripts.js
@@ -2,17 +2,14 @@
   Alert banner events
 */
 function toggleAlert () {
-  console.log('1');
   $('.alert').addClass('show');
   setTimeout(function () {
-    console.log('2');
     $('.alert').removeClass('show');
   }, 2000);
   return false;
 }
 
 $('.closeAlertBtn').on('click', function () {
-  console.log('3');
   $('.alert').removeClass('show');
 });
 
--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,19 @@
 window.setInterval(function ToggleButton () {
   if($('#inputcaptcha').val() == 'Delete Account') {
-    $('#deletebtn').removeClass('disabled');
+    $('#deletebtn').prop('disabled',false);
   } else {
-    $('#deletebtn').addClass('disabled');
+    $('#deletebtn').prop('disabled',true);
   }
 },200);
+
+function validateForm() {
+  var newpassword = document.forms["passwordform"]["newpassword"].value;
+  var repeat = document.forms["passwordform"]["newpasswordrepeat"].value;
+  if(newpassword === repeat) {
+    return true;
+  }
+  else {
+    alert("Please input your new password correctly");
+    return false;
+  }
+}
