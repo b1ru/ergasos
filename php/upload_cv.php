245c537cd8f2d4ce64369676744718cdf123f3fb
Achilleas Toumpas <atoumpas@csd.auth.gr>
2020-06-20 02:27:17 +0300

    Upload CV fixed

--- php/upload_cv.php
+++ php/upload_cv.php
@@ -1,13 +1,17 @@
 <?php
-  $target_dir = "../uploads/";
-  $newfilename = "cv_1.pdf";
+  if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+  }
+  $id = $_SESSION['id'];
+  $target_dir = "uploads/";
+  $newfilename = 'cv_'.$id.'.pdf';
   $target_file = $target_dir . $newfilename;
   $finfo = finfo_open(FILEINFO_MIME_TYPE);
   $imageFileType = finfo_file($finfo, $_FILES['cvfile']['tmp_name']);
   if($imageFileType == "application/pdf") {
-    if(move_uploaded_file($_FILES["cvfile"]["tmp_name"],$target_file)) {
+    if(move_uploaded_file($_FILES["cvfile"]["tmp_name"],'../'.$target_file)) {
       $con = mysqli_connect("localhost","root","","ergasos");
-      $query = "UPDATE account_details SET cv='uploads/cv_1.pdf' WHERE userID = 1";
+      $query = "UPDATE account_details SET cv='$target_file' WHERE userID = '$id'";
       if(mysqli_query($con,$query)) {
         setcookie("upload_cv_success","true",time() + 10,"/");
       }
