9683003ccf934f368d76bb0000a857daf7bbb661
Bill Tsek <billtsekev@gmail.com>
2020-06-19 20:09:10 +0300

    navbar changing with php

--- php/navbar.php
+++ php/navbar.php
@@ -1,4 +1,16 @@
-		<?php
+<?php
+	if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+	}
+
+	if (! isset($_SESSION['logged_in'])) {
+		$_SESSION['logged_in'] = false;
+	}
+
+	$_SESSION['logged_in'] = false;
+
+	if ($_SESSION['logged_in'] == false) {
+
 		echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<a href="./index.html">
 				<img src="images/bottlemsg.png">
@@ -26,9 +38,41 @@
 					</a>
 				</div>
   		</div>
-		</nav>
+		</nav>';
 
-		<span id="toggle-nav-container">
-			<a id="toggle-nav" href="#">Toggle NavBar Layout</a>
-		</span>';
-		?>
+	} 
+	else 
+	{
+		echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a href="./index.html">
+				<img src="images/bottlemsg.png">
+			</a>
+  		<a class="navbar-brand" href="./index.html">ErgaSOS</a>
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  		  <span class="navbar-toggler-icon"></span>
+  		</button>
+
+  		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  		  <ul class="navbar-nav mr-auto">
+  		    <li class="nav-item active">
+						<form class="form-inline my-2 my-lg-0" action="./search.html">
+		    		  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		    		  	<button id="search-button" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    		</form>
+  		    </li>
+  		  </ul>
+			<div>
+				<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="./images/user.svg" alt="user icon"></button>
+  		<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+  	  	<a class="dropdown-item" href="./my_profile.html">Profile</a>
+  	  	<a class="dropdown-item" href="./my_applications.html">My Applications</a>
+  	  	<a class="dropdown-item" href="./my_listings.html">My Listings</a>
+  	  	<a class="dropdown-item" href="./settings.html">Settings</a>
+				<div class="dropdown-divider"></div>
+  	  	<a id="logout-link" class="dropdown-item" href="#">Logout</a>
+  		</div>
+			</div>
+  		</div>
+		</nav>';
+}
+?>
