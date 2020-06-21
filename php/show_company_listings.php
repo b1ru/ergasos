97b9bbf3faecd115c6e57daa58c85f3bac760c53
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-22 02:25:56 +0300

    Update show_company_listings.php

--- php/show_company_listings.php
+++ php/show_company_listings.php
@@ -5,17 +5,17 @@ if (session_status() == PHP_SESSION_NONE) {
 $name=filter_input(INPUT_GET, 'name');
 $con = mysqli_connect("localhost","root","","ergasos");
 $query =
-"SELECT title,COUNT(listingID),description,listingID
+"SELECT title,description,listingID
 FROM listing_details
 WHERE creator = '$name'
 ORDER BY listingID DESC";
 $result = mysqli_query($con,$query);
 $num = mysqli_num_rows($result);
-if($num==0){
+if(mysqli_num_rows($result)==0){
     echo '<h1 style="text-align:center"> <br><br> This company has no listings. </h1>';
   }
 else{
-for($i = 0; $i < $num; $i++){
+for($i = 0; $i < mysqli_num_rows($result); $i++){
   $row = mysqli_fetch_row($result);
 
   echo '<div class="card">
@@ -25,12 +25,9 @@ for($i = 0; $i < $num; $i++){
     <div class="card-body">
       <h5 class="card-title">'.$row[0].'</h5>
       <p class="card-text">
-         <ul>
-            <li>'.$row[2].'</li>
-        </ul>
+         '.$row[1].'
       </p>
-      <span class="left"> <a href="./aggelia.html?id='.$row[3].'" class="btn btn-primary">View Details</a> </span>
-      <span class="right"> <button type = "button" class="btn btn-danger" data-toggle="modal" data-target="#myModal">Close listing</button> </span>
+      <span class="left"> <a href="./aggelia.html?id='.$row[2].'" class="btn btn-primary">View Details</a> </span>
     </div>
   </div>';
 
