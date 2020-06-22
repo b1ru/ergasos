2febf236981044f9fd8bc041a3e8664d9aaa9048
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-22 18:37:49 +0300

    Update profile_alerts.php

--- php/profile_alerts.php
+++ php/profile_alerts.php
@@ -51,4 +51,26 @@
     }
     setcookie("upload_cv_success","",time() - 3600,'/');
   }
+
+  if(isset($_COOKIE['delete_cv_success'])) {
+    if($_COOKIE['delete_cv_success'] == 'true') {
+      echo '<!-- ALERT -->
+      <div class="alert alert-success fade" role="alert">
+      <strong>Your CV has been deleted successfully</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    else {
+      echo '<!-- ALERT -->
+      <div class="alert alert-danger fade" role="alert">
+      <strong>Something went wrong while deleting your CV.Please try again.</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    setcookie("delete_cv_success","",time() - 3600,'/');
+  }
 ?>
