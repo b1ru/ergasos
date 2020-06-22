74529920a8984a40ef20d4559b62e7489029f294
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-23 01:34:22 +0300

    Update profile_alerts.php

--- php/profile_alerts.php
+++ php/profile_alerts.php
@@ -73,4 +73,25 @@
     }
     setcookie("delete_cv_success","",time() - 3600,'/');
   }
+  if(isset($_COOKIE['successful_register'])) {
+    if($_COOKIE['successful_register'] == 'true') {
+      echo '<!-- ALERT -->
+      <div class="alert alert-success fade" role="alert">
+      <strong>You have been successfuly registered and signed in!</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    else {
+      echo '<!-- ALERT -->
+      <div class="alert alert-danger fade" role="alert">
+      <strong>Something went wrong while registering.Please try again.</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    setcookie("successful_register","",time() - 3600,'/');
+  }
 ?>
