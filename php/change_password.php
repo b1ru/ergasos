5267286ddc5b4fee377dab1a136c1426805cc253
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-23 00:43:00 +0300

    Update change_password.php

--- php/change_password.php
+++ php/change_password.php
@@ -11,10 +11,11 @@
   $con = mysqli_connect("localhost","root","","ergasos");
   $oldpassword = mysqli_real_escape_string($con,$_POST['oldpassword']);
   $newpassword = mysqli_real_escape_string($con,$_POST['newpassword']);
+  $confirmpassword = mysqli_real_escape_string($con,$_POST['newpasswordrepeat']);
   $passwordquery = "SELECT password FROM login WHERE userID = '$id'";
   $results = mysqli_query($con,$passwordquery);
   $row = mysqli_fetch_row($results);
-  if(password_verify($oldpassword, $row[0])){
+  if(password_verify($oldpassword, $row[0]) && $newpassword==$confirmpassword){
     $newpassword=password_hash($newpassword, PASSWORD_DEFAULT);
     $changepassword = "UPDATE login SET password='$newpassword' WHERE userID = '$id'";
     if(mysqli_query($con,$changepassword)) {
