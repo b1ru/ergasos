2a4cdda743a1d68c5ed4addd91e7b74055713657
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-22 03:48:36 +0300

    Add files via upload

---
+++ php/delete_cv.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+	if(!isset($_SESSION['id'])){
+		header("Location: ./sign-in.html");
+	}
+	else {
+		$id = $_SESSION['id'];
+	}
+	$con = mysqli_connect("localhost","root","","ergasos");
+	$query="UPDATE account_details SET cv=NULL WHERE userID='$id'";
+	if (mysqli_query($con, $query)) {
+		unlink("uploads/cv_".$id);
+		header("Location: {$_SERVER["HTTP_REFERER"]}");
+	} 
+	else {
+		echo "Error updating record: " . mysqli_error($con);
+	}
+?>
