b454b96729b898e8e0e6c03fb51c2fededbb76c2
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-22 17:28:33 +0300

    Update applications_alerts.php

--- php/applications_alerts.php
+++ php/applications_alerts.php
@@ -20,4 +20,25 @@
     }
     setcookie("application_delete_success","",time() - 3600,'/');
   }
+  if(isset($_COOKIE['application_submit_success'])){
+    if($_COOKIE['application_submit_success'] == 'true'){
+      echo '<!-- ALERT -->
+      <div class="alert alert-success fade" role="alert">
+      <strong>You have successfuly applied for the listing.</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    else {
+      echo '<!-- ALERT -->
+      <div class="alert alert-danger fade" role="alert">
+      <strong>Something went wrong while applying for a listing.Please try again.</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    setcookie("application_delete_success","",time() - 3600,'/');    
+  }
 ?>
