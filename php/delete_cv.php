f6c562b2648d8fd8aa9b5e6f453eca0b15579bb3
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-22 18:38:18 +0300

    Update delete_cv.php

--- php/delete_cv.php
+++ php/delete_cv.php
@@ -10,9 +10,11 @@ session_start();
 	$query="UPDATE account_details SET cv=NULL WHERE userID='$id'";
 	if (mysqli_query($con, $query)) {
 		unlink("../uploads/cv_".$id.".pdf");
+		setcookie("delete_cv_success","true",time() + 10,"/");
 		header("Location: {$_SERVER["HTTP_REFERER"]}");
 	} 
 	else {
+		setcookie("delete_cv_success","false",time() + 10,"/");
 		echo "Error updating record: " . mysqli_error($con);
 	}
 ?>
