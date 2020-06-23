b6fb3eaba235e35481f92af1574ff49fdb7f0ff4
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-23 17:45:12 +0300

    Update index_alert.php

--- php/index_alert.php
+++ php/index_alert.php
@@ -20,4 +20,25 @@
     }
     setcookie("user_delete_success","",time() - 3600,'/');
   }
+  if(isset($_COOKIE['listing_delete_success'])) {
+    if($_COOKIE['listing_delete_success'] == 'true') {
+      echo '<!-- ALERT -->
+      <div class="alert alert-success fade" role="alert">
+      <strong>You have been successfuly removed the listing!</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    else {
+      echo '<!-- ALERT -->
+      <div class="alert alert-danger fade" role="alert">
+      <strong>Something went wrong while removing a listing.Please try again.</strong>
+      <button type="button" class="close closeAlertBtn" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>';
+    }
+    setcookie("listing_delete_success","",time() - 3600,'/');
+  }  
 ?>
