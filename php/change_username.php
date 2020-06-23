460b972ce9bbb1fc67f0aa6a0e9c1d3366a30bc0
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-23 20:47:40 +0300

    Update change_username.php

--- php/change_username.php
+++ php/change_username.php
@@ -12,20 +12,27 @@
   $newusername = mysqli_real_escape_string($con,$_POST['newusername']);
   $currentpassword = mysqli_real_escape_string($con,$_POST['currentpassword']);
   $passwordquery = "SELECT password FROM login WHERE userID = '$id'";
+  $usernamexistsquery = "SELECT username FROM login WHERE username= '$newusername'";
+  $dupename = mysqli_query($con,$usernamexistsquery);
   $results = mysqli_query($con,$passwordquery);
   $row = mysqli_fetch_row($results);
-  if(password_verify($currentpassword, $row[0])){
-    $changeusername = "UPDATE login SET username='$newusername' WHERE userID = '$id'";
-    if (mysqli_query($con,$changeusername)) {
-      setcookie('username_change_success','true',time() + 10,'/');
-    }
-    else {
-      setcookie('username_change_success','false',time() + 10,'/');
-    }
+  if (mysqli_num_rows($dupename)>=1) {
+  	setcookie('username_taken','true',time() + 10,'/');
   }
-  else {
-    setcookie('username_change_success','false',time() + 10,'/');
-  }
+  else{
+	  if(password_verify($currentpassword, $row[0])){
+	    $changeusername = "UPDATE login SET username='$newusername' WHERE userID = '$id'";
+	    if (mysqli_query($con,$changeusername)) {
+	      setcookie('username_change_success','true',time() + 10,'/');
+	    }
+	    else {
+	      setcookie('username_change_success','false',time() + 10,'/');
+	    }
+	  }
+	  else {
+	    setcookie('username_change_success','false',time() + 10,'/');
+	  }
+	}
 
   header("Location: {$_SERVER["HTTP_REFERER"]}");
 ?>
