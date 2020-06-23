5ec4e0499d1befafc12f80400e177dad8ae54704
skolidis <36901114+skolidis@users.noreply.github.com>
2020-06-23 17:36:07 +0300

    Update delete_user.php

--- php/delete_user.php
+++ php/delete_user.php
@@ -12,17 +12,38 @@
 		$res=mysqli_query($con,$query);
 		$row = mysqli_fetch_assoc($res);
 		$id=$row['userID'];
+		$ok=1;
 
 		$query = "DELETE FROM login WHERE userID = '$id'";
     	mysqli_query($con,$query);
+    	if (mysqli_affected_rows($con) == 0 ) {
+	      $ok = 0;
+	    }
 
     	$query = "DELETE FROM account_details WHERE userID = '$id'";
     	mysqli_query($con,$query);
+    	if (mysqli_affected_rows($con) == 0 ) {
+	      $ok = 0;
+	    }
 
     	$query = "DELETE FROM applications WHERE userID = '$id'";
     	mysqli_query($con,$query);
+    	if (mysqli_affected_rows($con) == 0 ) {
+	      $ok = 0;
+	    }
 
     	$query = "DELETE listing_details,listings FROM listing_details INNER JOIN listings ON listing_details.listingID=listings.listingID WHERE listings.userID = '$id'";
     	mysqli_query($con,$query);
+    	if (mysqli_affected_rows($con) == 0 ) {
+	      $ok = 0;
+	    }
+	    if($ok == 1) {
+	      setcookie("user_delete_success","true",time() + 10,"/");
+	    }
+	    else {
+	      setcookie("user_delete_success","false",time() + 10,"/");
+	    }
+	    header('Location: ../index.php');
 	}
+
 ?>
